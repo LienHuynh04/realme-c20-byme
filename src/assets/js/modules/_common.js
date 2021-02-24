be27b5588290488f9c81a59afa385b893bab7d5d
LienHuynh07 <lien.huynh@gumiviet.com>
2021-02-24 20:43:28 +0700

    init source

---
+++ src/assets/js/modules/_common.js
@@ -0,0 +1,24 @@
+
+export default class Common {
+    /* ===================================
+     *  CONSTRUCTOR
+     * =================================== */
+    constructor(){
+        // This File Contain The Behavior Of All The Pages
+
+        this.bindEvents();
+    }
+
+
+    /* ===================================
+     *  EVENTS
+     * =================================== */
+    bindEvents(){
+        console.log('Run Common Hieu')
+    }
+
+    /* ===================================
+     *  METHODS
+     * =================================== */
+
+}
